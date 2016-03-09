5b36661473895cf9b99c71c1c1273ce78e799d53
hexinxin <514803310@qq.com>
2016-03-09 22:16:27 +0800

    v1.1

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ routes(app);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  next(err,require('./routes/index_js'));
 });
 
 // error handlers
